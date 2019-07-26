f41985fe13472c158aece287439ddd91be01ad07
donnasvinis <dvsvinis@mac.com>
2019-07-26 18:54:26 -0400

    addbird form and header button working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BirdComponent } from './bird/bird.component';
+import { AddBirdComponent } from './add-bird/add-bird.component';
 import { BirdListComponent } from './bird-list/bird-list.component';
 import { BirdFormComponent } from './bird-form/bird-form.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
-{ path: '', component: BirdComponent },
-{ path: 'birds', component: BirdListComponent },
-  { path: 'addbird', component: BirdFormComponent }
+  { path: '', component: BirdComponent },
+  { path: 'birds', component: BirdListComponent },
+  { path: 'addbird', component: AddBirdComponent },
+  { path: 'birdform', component: BirdFormComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { BirdComponent } from './bird/bird.component';
 import { BirdListComponent } from './bird-list/bird-list.component';
 import { BirdFormComponent } from './bird-form/bird-form.component';
 import { BirdService } from './service/bird-service.service';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AddBirdComponent } from './add-bird/add-bird.component';
 
 @NgModule({
   declarations: [
     AppComponent,
      BirdComponent,
      BirdListComponent,
-     BirdFormComponent
+     BirdFormComponent,
+     HeaderComponent,
+     LoginComponent,
+     LogoutComponent,
+     AddBirdComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +30,7 @@ import { BirdService } from './service/bird-service.service';
     FormsModule,
     HttpClientModule
   ],
-  providers: [BirdService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
