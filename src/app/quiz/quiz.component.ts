c9f1ca277af502274b247f572b288c9b0be504e4
donnasvinis <dvsvinis@mac.com>
2019-08-01 07:33:26 -0400

    added audioplayer

---
+++ src/app/quiz/quiz.component.ts
@@ -0,0 +1,24 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+})
+export class QuizComponent {
+  pageTitle = 'Quiz Bird List';
+  // quizlist: any[] = [
+  //   {
+  //     id: 1,
+  //     name: 'Cardinal',
+  //     image: 'https://download.ams.birds.cornell.edu/api/v1/asset/169049481/480',
+  //     song1: 'https://www.allaboutbirds.org/guide/assets/sound/523097.mp3'
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'American Robin',
+  //     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/American_Robin_Close-Up.JPG/1024px-American_Robin_Close-Up.JPG',
+  //     song1: 'https://www.https://www.allaboutbirds.org/guide/assets/sound/508723.mp3.org/guide/assets/sound/523097.mp3'
+  //   },
+  // ];
+
+}
