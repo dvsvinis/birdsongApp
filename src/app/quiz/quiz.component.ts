fc26d3645713fc5f54d17d8f39e6e8120cf8f268
donnasvinis <dvsvinis@mac.com>
2019-08-05 16:41:41 -0400

    added listOfBirdNames and method to compare to DropList - method working

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -12,18 +12,35 @@ export class QuizComponent {
   // pageTitle = 'Quiz Bird List';
   birds: Bird[];
   DropList = ['American Robin', 'Gray Catbird', 'Common Grackle', 'Northern Cardinal', 'Black Capped Chickadee'];
+  listOfBirdNames: string[];
 
   constructor(private birdService: BirdService) { }
 
   ngOnInit() {
     this.birdService.findAll().subscribe(data => {
+      console.log("data: " + data[0].name);
       this.birds = data;
+      this.listOfBirdNames = this.birds.map(bird => {
+        return bird.name;
+      });
+      console.log("birds: " + this.birds[0].name);
     });
+
   }
 
-  onDrop(event){
+  onDrop(event) {
     console.log(event);
     moveItemInArray(this.DropList, event.previousIndex, event.currentIndex);
+    this.nameCompare();
+  }
+
+  nameCompare() {
+    this.DropList.forEach((item, index) => {
+      if (item !== this.listOfBirdNames[index]) {
+          // raise of flag saying their answer is wrong
+          console.log("This not right");
+      }
+    });
   }
 
 }
