08e5ba4547c55f19370c0fc1c2002b65589c7798
donnasvinis <dvsvinis@mac.com>
2019-08-07 09:12:14 -0400

    bird images drag and drop set up

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -11,9 +11,16 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 export class QuizComponent {
   // pageTitle = 'Quiz Bird List';
   birds: Bird[];
-  DropList = ['American Robin', 'Gray Catbird', 'Common Grackle', 'Northern Cardinal', 'Black Capped Chickadee'];
+  nameDropList = ['American Robin', 'Gray Catbird', 'Common Grackle', 'Northern Cardinal', 'Black Capped Chickadee'];
   listOfBirdNames: string[];
-  showAlert: boolean = false;
+  listOfBirdImages: string[];
+  showAlert: boolean;
+  // imageDropList: string[];
+  imageDropList = ['https://upload.wikimedia.org/wikipedia/commons/c/c9/Black-capped_Chickadee_%284436797251%29.jpg',
+  'https://download.ams.birds.cornell.edu/api/v1/asset/169049481/480',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/American_Robin_Close-Up.JPG/1024px-American_Robin_Close-Up.JPG',
+  'https://upload.wikimedia.org/wikipedia/commons/3/34/Gray_Catbird_%2817166164877%29.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/b/b1/Common_Grackle_%2827182592494%29.jpg'];
 
   constructor(private birdService: BirdService) { }
 
@@ -23,21 +30,35 @@ export class QuizComponent {
       this.listOfBirdNames = this.birds.map(bird => {
         return bird.name;
       });
+      this.listOfBirdImages = this.birds.map(bird => {
+        return bird.image;
+      });
     });
 
   }
 
-  onDrop(event) {
+  onNameDrop(event) {
     console.log(event);
-    moveItemInArray(this.DropList, event.previousIndex, event.currentIndex);
-    if (this.DropList[event.currentIndex] === this.listOfBirdNames[event.currentIndex]) {
+    moveItemInArray(this.nameDropList, event.previousIndex, event.currentIndex);
+    if (this.nameDropList[event.currentIndex] === this.listOfBirdNames[event.currentIndex]) {
       alert ('Success - this is a match!');
-      // this.showAlert = true;
+      this.showAlert = true;
       console.log(this.showAlert);
-    } else if (this.DropList[event.currentIndex] !== this.listOfBirdNames[event.currentIndex]){
+    } else if (this.nameDropList[event.currentIndex] !== this.listOfBirdNames[event.currentIndex]) {
       alert ('Try again!');
     }
-    this.showAlert = false;
+  }
+
+  onImageDrop(event) {
+    console.log(event);
+    moveItemInArray(this.imageDropList, event.previousIndex, event.currentIndex);
+    if (this.imageDropList[event.currentIndex] === this.listOfBirdImages[event.currentIndex]) {
+      alert ('Success - this is a match!');
+      this.showAlert = true;
+      console.log(this.showAlert);
+    } else if (this.imageDropList[event.currentIndex] !== this.listOfBirdImages[event.currentIndex]) {
+      alert ('Try again!');
+    }
   }
 
   // nameCompare() {
@@ -54,5 +75,9 @@ export class QuizComponent {
 
   // }
 
-
+  // 'https://download.ams.birds.cornell.edu/api/v1/asset/169049481/480';
+  // ('https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/American_Robin_Close-Up.JPG/1024px-American_Robin_Close-Up.JPG');
+  // ('https://upload.wikimedia.org/wikipedia/commons/3/34/Gray_Catbird_%2817166164877%29.jpg');
+  // ('https://upload.wikimedia.org/wikipedia/commons/c/c9/Black-capped_Chickadee_%284436797251%29.jpg');
+  // ('https://upload.wikimedia.org/wikipedia/commons/b/b1/Common_Grackle_%2827182592494%29.jpg');
 }
